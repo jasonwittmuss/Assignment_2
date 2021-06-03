4d68895749e0ed650d688d58ce4a9ca3fd89779a
jasonwittmuss <85248413+jasonwittmuss@users.noreply.github.com>
2021-06-03 13:05:42 -0500

    Add files via upload

---
+++ src/com/ss/jn/two/Assignment_2_2.java
@@ -0,0 +1,71 @@
+package com.ss.jn.two;
+
+import java.util.Random;
+
+public class Assignment_2_2 {
+	public static void main(String[] args) {
+		// Call to construct an array with the hard coded parameters
+		int[][] arr = constructArray(10, 10, 100);
+
+		// Print array to help with verification
+		printArray(arr);
+
+		// Find and print the max value and position
+		int[] best = findMax(arr);
+		int maxint = best[0];
+		int posx = best[1];
+		int posy = best[2];
+
+		System.out.println("The max number is " + maxint + " and is found in position (" + posx + "," + posy + ")");
+	}
+
+	public static void printArray(int[][] arr) {
+		// Loops through the array and prints it out with formatting
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length - 1; j++) {
+				if (arr[i][j] < 10) {
+					System.out.print(" " + arr[i][j] + ", ");
+				} else {
+					System.out.print(arr[i][j] + ", ");
+				}
+			}
+			if (arr[i][arr[i].length - 1] < 10) {
+				System.out.println(" " + arr[i][arr[i].length - 1]);
+			} else {
+				System.out.println(arr[i][arr[i].length - 1]);
+			}
+		}
+	}
+
+	public static int[][] constructArray(int x, int y, int max) {
+		// Randomly assigns a new value to each position in the array
+		Random rand = new Random();
+
+		int[][] arr = new int[x][y];
+
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				arr[i][j] = rand.nextInt(max);
+			}
+		}
+
+		return arr;
+	}
+
+	public static int[] findMax(int[][] arr) {
+		// Loops through the array keeping and updating the max value and position
+		int[] best = { 0, -1, -1 };
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				if (arr[i][j] > best[0]) {
+					best[0] = arr[i][j];
+					best[1] = i;
+					best[2] = j;
+				}
+			}
+		}
+
+		return best;
+	}
+}
---
+++ src/com/ss/jn/two/Assignment_2_3/Circle.java
@@ -0,0 +1,24 @@
+package com.ss.jn.two.Assignment_2_3;
+
+public class Circle implements Shape {
+	// Circle class
+	Double radius;
+
+	public Circle(Double radius) {
+		// To define a Circle we need a radius
+		this.radius = radius;
+	}
+
+	@Override
+	public Double calculateArea() {
+		// Area is PI*r^2
+		return (this.radius * this.radius * Math.PI);
+	}
+
+	@Override
+	public void display() {
+		// Calling calculate area to display area
+		System.out.println(calculateArea());
+	}
+
+}
