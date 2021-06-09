318597a3fe0a9d41406f96f511acf1b0f1a92fb1
jasonwittmuss <85248413+jasonwittmuss@users.noreply.github.com>
2021-06-09 15:12:24 -0500

    Delete Shape.java

--- src/com/ss/jn/two/Assignment_2_3/Shape.java
+++
@@ -1,9 +0,0 @@
-package com.ss.jn.two.Assignment_2_3;
-
-public interface Shape {
-	// Interface that give the methods needed for a shape
-
-	public Double calculateArea();
-
-	public void display();
-}
